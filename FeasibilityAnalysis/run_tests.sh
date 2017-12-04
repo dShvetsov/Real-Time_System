aa7898f10997ec7f63903361a710c0da4dbfd657
Denis Shvetsov <dshvetsov@arccn.ru>
2017-12-04 09:47:26 +0300

    run test

--- FeasibilityAnalysis/run_tests.sh
+++ FeasibilityAnalysis/run_tests.sh
@@ -8,11 +8,14 @@ fi
 yesno_single_test() {
     test_file=$1
     expected_answer=$2
-    if [ -n $($program $test_file | grep $expected_answer)  ] ; then
-        echo "OK"
+    if [ -n "$($program $test_file | grep $expected_answer)"  ] ; then
+        result="\033[32m OK \033[0m"
+        ret=0
     else
-        echo "FAILED"
+        result="\033[31m FAILED \033[0m"
+        ret=1
     fi
+    echo -en "Test : $test_file : [ $result ] \n"
     return 0
 }
 
@@ -21,12 +24,22 @@ yesno_test_dir() {
     expected=$2
     for f in $(ls ${directory}/*.xml) ; do
         yesno_single_test $f $expected
+        if [ $? -ne 0 ] ; then
+            let failed=$failed+1
+        fi
+        let total=$total+1
     done
 }
 
-program=$1
+export program=$1
 test_dir="testdata"
+export total=0
+export failed=0
 
+echo "Test not feasible"
 yesno_test_dir ${test_dir}/NO "NO"
+echo "Test feasible"
 yesno_test_dir ${test_dir}/YES "YES"
 
+echo "------------------------------"
+echo "Total : $total, Failed : $failed"
